c69c8b13c72b21d546c24e499fad2019860c7029
陈刚 <1670755868@qq.com>
2021-07-01 19:13:35 +0800

    自动化部署02

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,8 @@ npm install
 # 打包
 npm run build
 # 删除xianyu-mobile-pages目录下public目录中的文件
-rm -rf /web/xianyu-mobile-pages/public/
+rm -rf /web/xianyu-mobile-pages/public/*
 # 将打包好的文件拷贝过去
-mv /tmp/xianyu-mobile/dist/* /web/xianyu-mobile-pages/public
+mv /tmp/xianyu-mobile/dist/* /web/xianyu-mobile-pages/public/
 cd /web/xianyu-mobile-pages
 pm2 restart index.js
-# 删除clone的代码
-rm -rf /tmp/xianyu-mobile
