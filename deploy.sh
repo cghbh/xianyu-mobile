a847cbb6743916fce578c9755c41b7a5977e537a
陈刚 <1670755868@qq.com>
2021-07-01 19:19:50 +0800

    自动化部署03

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ npm run build
 # 删除xianyu-mobile-pages目录下public目录中的文件
 rm -rf /web/xianyu-mobile-pages/public/*
 # 将打包好的文件拷贝过去
-mv /tmp/xianyu-mobile/dist/* /web/xianyu-mobile-pages/public/
+mv /tmp/xianyu-mobile/dist/ /web/xianyu-mobile-pages/public/
 cd /web/xianyu-mobile-pages
 pm2 restart index.js
